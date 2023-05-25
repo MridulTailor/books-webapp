016fa2772b237f46a5dd12dcf11da30fe8621dce
MridulTailor <mtailor729@gmail.com>
2023-05-25 14:04:25 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,43 @@
 // import logo from './logo.svg';
-import "./App.css";
-import { Home } from "./pages/Home";
-import { User } from "./pages/User";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { ThemeProvider } from "@emotion/react";
-import { theme } from "./style.js";
-function App() {
+import './App.css';
+import { Link } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { Home } from './components/Home';
+import { Register } from './components/Register';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <div>
-          <Link to="/" style={{ marginLeft: 5 }}>
-            Home
-          </Link>
-          <Link to="/user" style={{ marginLeft: 10 }}>
-            User
-          </Link>
+    <BrowserRouter>
+        <div style={{
+          display:"flex",
+          alignItems:"center",
+          justifyContent:"center",
+          marginTop:30,
+          marginBottom:20
+        }}>
+          <Link to="/" style={{
+            marginLeft:5,
+            textDecoration:"none",
+            fontSize:17,
+            color:'#2E2E2E'
+            }}>Home</Link>
+          <ArrowForwardIosIcon style={{
+            fontSize:17,
+            color:"#2E2E2E",
+            textAlign:"center"
+
+          }}/>
+          <Link to="/register" style={{
+            textDecoration:"none",
+            fontSize:17,
+            color:'rgb(255,89,92)'
+            }}>Create an Account</Link>
         </div>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
-          <Route path="/user" element={<User />}></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/register" element={<Register/>}></Route>
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
   );
 }
 
